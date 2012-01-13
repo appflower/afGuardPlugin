29e270a9415ef791f6283789867af429d14f7e51
Nikolai Babinski <babinski.nikolai@gmail.com>
2012-01-13 13:19:57 +0200

    removed text->password field transformation "PasswordFocus" property, this transformation raise an error in IE

--- modules/afGuardAuth/templates/passwordResetSuccess.php
+++ modules/afGuardAuth/templates/passwordResetSuccess.php
@@ -14,8 +14,8 @@ $col = $columns->startColumn(array("columnWidth"=>1));
 $layout->addHelp('Thanks for confirming your email address. You may now change your password using the form below.');	
 $username=new afExtjsFieldHidden($col,array('name'=>'uid','value'=>$sf_params->get('uid')));
 $validate=new afExtjsFieldHidden($col,array('name'=>'validate','value'=>$sf_params->get('validate')));
-$password=new afExtjsFieldInput($col,array('name'=>'password','label'=>'Password','value'=>'','help'=>"Enter the password",'emptyText'=>'write your password','width'=>'150','PasswordFocus'=>'true'));
-$password=new afExtjsFieldInput($col,array('name'=>'confirm_password','label'=>'Confirm Password','value'=>'','help'=>"The re-typed password, to ensure that it has been entered correctly.",'emptyText'=>'re-type your password','width'=>'150','PasswordFocus'=>'true'));
+$password=new afExtjsFieldPassword($col,array('name'=>'password','label'=>'Password','value'=>'','help'=>"Enter the password",'width'=>'150'));
+$password=new afExtjsFieldPassword($col,array('name'=>'confirm_password','label'=>'Confirm Password','value'=>'','help'=>"The re-typed password, to ensure that it has been entered correctly.",'width'=>'150'));
 
 $columns->endColumn($col);
 $fieldset->endColumns($columns);
